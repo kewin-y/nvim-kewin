57d0181fd382a250f7b72d369891597c64ff0301
Kevin <jekevin964@gmail.com>
2025-04-11 15:10:42 -0400

    what am i doing

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -11,4 +11,20 @@ return {
     lazy = true,
     version = false
   },
+  {
+    'echasnovski/mini.starter',
+    opts = {
+      evaluate_single = true,
+      header = "hello",
+      content_hooks = {
+        require('mini.starter').gen_hook.aligning('center', 'center'),
+      },
+      items = {
+        require('mini.starter').sections.builtin_actions(),
+        require('mini.starter').sections.recent_files(5, true, false),
+        require('mini.starter').sections.recent_files(5, false, false),
+      },
+    },
+    version = false
+  },
 }
