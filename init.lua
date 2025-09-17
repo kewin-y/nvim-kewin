649a1b02d07b6df23bf90c7c4defd730a08b316f
Kevin <jekevin964@gmail.com>
2025-09-17 15:46:33 -0400

    refactor autocmds

--- init.lua
+++ init.lua
@@ -7,33 +7,5 @@ require("config.opts")
 require("config.keybinds")
 require("config.lazy")
 require("config.ui")
+require("config.autocmds")
 
-local format_group = vim.api.nvim_create_augroup("Format", {})
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = { "*" },
-    group = format_group,
-    callback = function(_)
-        local save_cursor = vim.fn.getpos(".")
-        vim.cmd([[%s/\s\+$//e]])
-        vim.fn.setpos(".", save_cursor)
-    end,
-})
-
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-    pattern = { "*.c", "*.h", "*.cpp", "*.hpp" },
-    group = format_group,
-    callback = function(_)
-        vim.opt.shiftwidth = 8
-        vim.opt.tabstop = 8
-    end,
-})
-
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-    pattern = { "*.nix" },
-    group = format_group,
-    callback = function(_)
-        vim.opt.shiftwidth = 2
-        vim.opt.tabstop = 2
-    end,
-})
---
+++ lua/config/autocmds.lua
@@ -0,0 +1,38 @@
+local format_group = vim.api.nvim_create_augroup("Format", {})
+local spell_group = vim.api.nvim_create_augroup("spell", {})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = { "*" },
+    group = format_group,
+    callback = function(_)
+        local save_cursor = vim.fn.getpos(".")
+        vim.cmd([[%s/\s\+$//e]])
+        vim.fn.setpos(".", save_cursor)
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+    pattern = { "*.c", "*.h", "*.cpp", "*.hpp" },
+    group = format_group,
+    callback = function(_)
+        vim.opt.shiftwidth = 8
+        vim.opt.tabstop = 8
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+    pattern = { "*.nix" },
+    group = format_group,
+    callback = function(_)
+        vim.opt.shiftwidth = 2
+        vim.opt.tabstop = 2
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+    pattern = { "*.typ" },
+    group = spell_group,
+    callback = function(_)
+        vim.cmd("setlocal spell spelllang=en_ca")
+    end,
+})
