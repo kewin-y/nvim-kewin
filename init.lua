f85c5b0722bbe261c4270c22496dd6fc581ff1fe
Kevin <jekevin964@gmail.com>
2025-12-07 18:08:07 -0500

    theme overhaul -- remove everything, add decaf

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-_G.theme = "cyberdream"
+_G.theme = "decaf"
 
 require("config.opts")
 require("config.keybinds")
--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -5,6 +5,7 @@ return {
         version = "1.*",
         event = { "InsertEnter", "CmdlineEnter" },
         lazy = true,
+
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
         opts = {
