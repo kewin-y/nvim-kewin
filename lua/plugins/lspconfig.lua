9aafcf24c81d8b8ac940ac43012e01c800398b11
Kevin <jekevin964@gmail.com>
2026-02-06 20:27:08 -0500

    change lsp keymaps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -18,6 +18,12 @@ return {
         event = { "BufReadPost", "BufNewFile" },
         cmd = { "LspInfo", "LspStart" },
         config = function()
+            vim.diagnostic.config({
+                virtual_lines = {
+                    current_line = true,
+                },
+            })
+
             local servers = {
                 "lua_ls",
                 "clangd",
@@ -40,28 +46,9 @@ return {
                 group = vim.api.nvim_create_augroup("KevinLsp", {}),
                 callback = function(_)
                     local lsp = vim.lsp.buf
-                    vim.keymap.set("n", "gd", lsp.definition, { desc = "Goto Definition" })
-                    vim.keymap.set("n", "gr", lsp.references, { desc = "Goto References" })
-                    vim.keymap.set("n", "gD", lsp.declaration, { desc = "Goto Declaration" })
-                    vim.keymap.set("n", "gi", lsp.implementation, { desc = "Goto Implementation" })
-                    vim.keymap.set("n", "gT", lsp.type_definition, { desc = "Type Definition" })
-                    vim.keymap.set("n", "K", lsp.hover, { desc = "Hover" })
-                    vim.keymap.set(
-                        "n",
-                        "<leader>cw",
-                        lsp.workspace_symbol,
-                        { desc = "Workspace Symbol" }
-                    )
-                    vim.keymap.set("n", "<leader>ra", lsp.rename, { desc = "Rename" })
-                    vim.keymap.set("n", "<leader>ca", lsp.code_action, { desc = "Code Action" })
-
-                    local diag = vim.diagnostic
-                    vim.keymap.set("n", "[d", function()
-                        diag.jump({ count = -1, float = true })
-                    end, { desc = "Next Diagnostic" })
-                    vim.keymap.set("n", "]d", function()
-                        diag.jump({ count = 1, float = true })
-                    end, { desc = "Previous Diagnostic" })
+                    vim.keymap.set("n", "grd", lsp.definition, { desc = "Goto Definition" })
+                    vim.keymap.set("n", "gre", lsp.declaration, { desc = "Goto Declaration" })
+                    vim.keymap.set("n", "gws", lsp.workspace_symbol, { desc = "Workspace Symbol" })
                 end,
             })
         end,
